eeefa3673b822e0a1a2e076a059a93a72a6f2021
Your Name <you@example.com>
2025-12-23 16:48:57 +0300

    added details page to deals

--- src/components/DealsOfTheDay/DealsOfTheDay.tsx
+++ src/components/DealsOfTheDay/DealsOfTheDay.tsx
@@ -83,7 +83,7 @@ const DealsOfTheDay = memo(function DealsOfTheDay() {
           </span>
         </div>
 
-        <Link href="/deals" className={styles.viewAll} aria-label="View all deals">
+        <Link href="/shop" className={styles.viewAll} aria-label="View all deals">
           View All Deals
           <svg className={styles.arrowIcon} viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
